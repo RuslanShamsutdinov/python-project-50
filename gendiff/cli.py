99e63a91a5943b9260576305552a699a96a4dd68
Ruslan Shamsutdinov <nexus191@yandex.ru>
2022-12-29 23:26:15 +0500

    1 step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,4 +1,9 @@
+import argparse
 
 
 def parser_args():
-    print("Hello world")
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    return parser.parse_args()
