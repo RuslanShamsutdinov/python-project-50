8f535be8c64a22d87e22ce7447a4ac6044886411
Ruslan Shamsutdinov <nexus191@yandex.ru>
2023-02-18 17:34:10 +0500

    8 step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,6 +6,7 @@ def parser_args():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', help='output format (default: "stylish")',
+    parser.add_argument('-f', '--format',
+                        help='output format (default: "stylish")',
                         default='stylish')
     return parser.parse_args()
