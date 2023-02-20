bf4011671b4a6740f231dabb5617779da6ac9c57
Ruslan Shamsutdinov <nexus191@yandex.ru>
2023-02-21 00:39:14 +0500

    8 step

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -5,6 +5,6 @@ import json
 
 def json_format(diff):
     json_line = json.dumps(diff_to_concatenated_dict(diff), indent=4)
-    replace_values = {',': '', '"': '', ': \n': ':\n'}
-    result = multiple_replace(json_line, replace_values)
-    return result
+    # replace_values = {',': '', '"': '', ': \n': ':\n'}
+    # result = multiple_replace(json_line, replace_values)
+    return json_line
